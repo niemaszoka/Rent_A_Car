c29dd3833a5a469fb5db96607cdd1c634186316d
ajakubas <arekjakubas@gmail.com>
2016-02-07 02:31:11 +0100

    Car rental form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use CarRental\Application\RentForm;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
 class DefaultController extends Controller
@@ -51,5 +54,40 @@ class DefaultController extends Controller
         return $this->render('default/form.html.twig', [
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..')
         ]);
+    }
+
+    public function rentFormAction(Request $request)
+    {
+        $Car=$_GET['CarID'];
+        $rentForm = new RentForm();
+        $rentForm->setCarID($_GET['CarID']);
+        $rentForm->setBrand($_GET['Brand']);
+        $rentForm->setSegment($_GET['Segment']);
+        $rentForm->setPricePerDay($_GET['PricePerDay']);
+
+        $form = $this->createFormBuilder($rentForm)
+            ->add('CarID', TextType::class)
+            ->add('Brand', TextType::class)
+            ->add('Segment', TextType::class)
+            ->add('PricePerDay', TextType::class)
+            ->add('RENT', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $Brand=$_GET['Brand'];
+            $ID=$_GET['CarID'];
+  
+            $file = dirname(__FILE__).'/Rented';
+            $json = json_decode(file_get_contents($file), true);
+            $json[$Brand] = array("rented"=>true);
+            file_put_contents($file, json_encode($json));
+            return $this->redirect($this->generateUrl('list'));
+        }
+
+        return $this->render('default/rent.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
